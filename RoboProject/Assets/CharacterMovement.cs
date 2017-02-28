8de2aa29fb3df225ae23dd4e81b84565e7d76449
Thomas Lu <thomastianlu@gmail.com>
2017-02-27 22:57:36 -0800

    Initial Setup
    Added basic character movement script
    Added basic toon shader
    Hooked up animations with basic character movement script

---
+++ RoboProject/Assets/CharacterMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float _movementSpeed;
+    [SerializeField]
+    private float _jumpHeight;
+
+    private Rigidbody _rigidbody;
+
+
+    [SerializeField]
+    private Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+        _rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ManageKeyboardControls();
+	}
+
+    void ManageKeyboardControls()
+    {
+        // Moving horizontal
+
+        float characterMagnitude = Input.GetAxis("Horizontal") * _movementSpeed;
+
+        _rigidbody.AddForce(Vector3.right * characterMagnitude);
+        _animator.SetFloat("MoveSpeed", characterMagnitude);
+
+        // Moving Vertical
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidbody.AddForce(Vector3.up * _jumpHeight);
+        }
+    }
+}
