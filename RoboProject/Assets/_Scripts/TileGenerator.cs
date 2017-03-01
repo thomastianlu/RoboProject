2fa0515c1fe3da8f54dfea045fb6b060c318d9b4
Thomas Lu <thomastianlu@gmail.com>
2017-02-28 19:38:27 -0800

    Created tile object pool that generates terrain

---
+++ RoboProject/Assets/_Scripts/TileGenerator.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class TileGenerator : MonoBehaviour {
+
+    [SerializeField]
+    private TileGeneratorDetector _topTileGenerator;
+    [SerializeField]
+    private TileGeneratorDetector _bottomTileGenerator;
+
+    [SerializeField]
+    private GameObject _tilePrefab;
+    [SerializeField]
+    private int _tilePoolAmount;
+    [SerializeField]
+    private List<GameObject> _tiles = new List<GameObject>();
+
+    [SerializeField]
+    private Transform _inactivePoolParent;
+    [SerializeField]
+    private Transform _activePoolParent;
+
+
+    // Use this for initialization
+    void Start () {
+        InitializeObjectPool();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageTileGeneration();
+    }
+
+    void InitializeObjectPool()
+    {
+        for (int i = 0; i < _tilePoolAmount; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(_tilePrefab);
+            obj.SetActive(false);
+            obj.transform.SetParent(_inactivePoolParent);
+            _tiles.Add(obj);
+        }
+    }
+
+    void ManageTileGeneration()
+    {
+        if (_topTileGenerator.shouldCreateTile)
+        {
+            GameObject newTile = GetPooledObject();
+            if (newTile)
+            {
+                newTile.transform.position = _topTileGenerator.transform.position;
+                newTile.SetActive(true);
+                _topTileGenerator.shouldCreateTile = false;
+            }
+        }
+
+        if (_bottomTileGenerator.shouldCreateTile)
+        {
+            GameObject newTile = GetPooledObject();
+            if (newTile)
+            {
+                newTile.transform.position = _bottomTileGenerator.transform.position;
+                newTile.SetActive(true);
+                _bottomTileGenerator.shouldCreateTile = false;
+            }
+        }
+    }
+
+    GameObject GetPooledObject()
+    {
+        foreach (GameObject x in _tiles)
+        {
+            if (!x.activeInHierarchy)
+            {
+                return x;
+            }
+        }
+
+        return null;
+    }
+
+}
--- RoboProject/Assets/_Scripts/TiledFloorAnimatorScript.cs
+++ RoboProject/Assets/_Scripts/TiledFloorAnimatorScript.cs
@@ -5,9 +5,12 @@ public class TiledFloorAnimatorScript : MonoBehaviour {
 
     [SerializeField]
     private GameObject _parent;
-	
+    [SerializeField]
+    private Animator _animator;
+
     void ReturnToObjectPool()
     {
+        _animator.Play("Idle");
         _parent.SetActive(false);
     }
 }
