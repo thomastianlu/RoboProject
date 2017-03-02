e4b650d17e2540e3dbaa724bb1fdd14a2a858292
Thomas Lu <thomastianlu@gmail.com>
2017-03-02 02:17:14 -0800

    Added title screen and game over screen

---
+++ RoboProject/Assets/BossTitleAnimationPicker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossTitleAnimationPicker : MonoBehaviour {
+
+    [SerializeField]
+    private Animator _animator;
+    private float _timeToPickNewAnimation = 5f;
+    private float _timeToPickNewAnimationReset;
+
+	// Use this for initialization
+	void Start () {
+        _timeToPickNewAnimationReset = _timeToPickNewAnimation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            _timeToPickNewAnimation -= Time.deltaTime;
+
+            if (_timeToPickNewAnimation < 0)
+            {
+                _animator.SetInteger("AnimationPick", Random.Range(0, 6));
+                _timeToPickNewAnimation = _timeToPickNewAnimationReset;
+            }
+        }
+	}
+}
---
+++ RoboProject/Assets/GameUIControlsManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameUIControlsManager : MonoBehaviour {
+    [SerializeField]
+    private bool _shouldRestartGame;
+    private float _screenStayMinimum = 1f;
+
+	// Update is called once per frame
+	void Update () {
+        ManageInputs();
+	}
+
+    void ManageInputs()
+    {
+        _screenStayMinimum -= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) 
+            && _screenStayMinimum < 0
+            && !_shouldRestartGame)
+        {
+            int buildIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(buildIndex + 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space)
+            && _screenStayMinimum < 0
+            && _shouldRestartGame)
+        {
+            int buildIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(0);
+        }
+    }
+}
